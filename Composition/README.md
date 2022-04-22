2d4240cca0aadddc57784116d4c5a5522dc29076
Xuhang Chen <yc17491@umac.mo>
2022-04-22 17:30:42 +0800

    allow multiple

--- Composition/README.md
+++ Composition/README.md
@@ -16,12 +16,16 @@ Composition
 
 ## Command to run the program.
 ```python
-python alpha.py --alpha_min min_alpha --alpha_max max_alpha --height image_height --width image_width --mode align or random
+python alpha.py --alpha_min min_alpha --alpha_max max_alpha --height image_height --width image_width --mode align or random --num_shadow only available in random mode
 ```
 ### Note
-min_alpha and max_alpha are between 0 and 1.
-align mode means that your shadow-free and shadow mask form 1 to 1 pair and they share the same name and format.
-random mode will pick random mask from the mask folder and combine with input file.
+min_alpha and max_alpha are between 0 and 1
+
+align mode means that your shadow-free and shadow mask form 1 to 1 pair and they share the same name and format
+
+random mode will pick random mask from the mask folder and combine with input file
+
+num_shadow generate multiple shadow images at a time, only available in random mode
 
 ## Example input
 ![input](./example_input.png)
--- Composition/alpha.py
+++ Composition/alpha.py
@@ -2,8 +2,25 @@ from turtle import back
 import cv2
 import numpy as np
 import argparse
+from sys import argv
 import random
 import os
+
+def save_shadow(mask_img, filename):
+    mask_img = cv2.resize(mask_img, (width, height))
+    shadow = 255 - mask_img
+    output = np.zeros((height, width, 3))
+    alpha = random.uniform(args.alpha_min, args.alpha_max)
+    for h in range(0, height):
+        for w in range(0, width):
+            if shadow[h][w][0] == 255:
+                output[h][w] = inp_img[h][w]
+            else:
+                output[h][w] = alpha * inp_img[h][w] + \
+                    (1 - alpha) * shadow[h][w]
+    cv2.imwrite(os.path.join('output', filename), output)
+
+mode = ['align', 'random']
 
 parser = argparse.ArgumentParser(description='This is the composition method to generate shadow')
 parser.add_argument('--alpha_min', type=int, default=0.4, help='Minimum alpha value')
@@ -14,6 +31,8 @@ parser.add_argument('--width', type=int, default=256,
                     help='Image width')
 parser.add_argument('--mode', type=str, default='align',
                     help='Minimum alpha value', choices=['align', 'random'])
+parser.add_argument('--num_shadow', type=int, default=1,
+                    help='Number of shadows', required=(mode[1] in argv))
 args = parser.parse_args()
 
 height = args.height
@@ -27,17 +46,15 @@ for inp in inputs:
     inp_img = cv2.resize(inp_img, (width, height))
     if args.mode == 'align':
         mask_img = cv2.imread(os.path.join('mask', inp))
+        save_shadow(mask_img, inp)
     else:
-        mask_img = cv2.imread(os.path.join('mask', random.choice(masks)))
-    mask_img = cv2.resize(mask_img, (width, height))
-    shadow = 255 - mask_img
-    output = np.zeros((height, width, 3))
-    alpha = random.uniform(args.alpha_min, args.alpha_max)
-    for h in range(0, height):
-        for w in range(0, width):
-            if shadow[h][w][0] == 255:
-                output[h][w] = inp_img[h][w]
-            else:
-                output[h][w] = alpha * inp_img[h][w] + \
-                    (1 - alpha) * shadow[h][w]
-    cv2.imwrite(os.path.join('output', inp), output)
+        sampled_shadows = random.sample(masks, args.num_shadow)
+        cnt = 1
+        for shadow in sampled_shadows:
+            mask_img = cv2.imread(os.path.join('mask', shadow))
+            save_shadow(mask_img, str(cnt) + '_shadow_' + inp)
+            cnt += 1
+
+    
+
+
