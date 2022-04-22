9abafaad964cc20c28dd793de42f80e9604de04c
Xuhang Chen <yc17491@umac.mo>
2022-04-22 16:04:58 +0800

    update

--- Generation/gen_shadow.py
+++ Generation/gen_shadow.py
@@ -1,27 +1,45 @@
 import utils as utils
-import datasets as dts
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import random
 import numpy as np
 import cv2
-import torch
+import tensorflow as tf
+import argparse
+import os
 
-# tf.compat.v1.enable_eager_execution(
-#     config=None, device_policy=None, execution_mode=None
-# )
+parser = argparse.ArgumentParser(
+    description='This is the composition method to generate shadow')
+parser.add_argument('--min_val', type=int, default=0.7,
+                    help='Render per-pixel intensity variation mask within [min_val, 1.]')
+parser.add_argument('--height', type=int, default=256,
+                    help='Image height')
+parser.add_argument('--width', type=int, default=256,
+                    help='Image width')
+args = parser.parse_args()
+
+tf.compat.v1.enable_eager_execution(
+    config=None, device_policy=None, execution_mode=None
+)
 TONE_SIGMA = 0.1
 SS_SIGMA = 0.5
 
-size = (256, 256)
+size = (args.height, args.width)
 
-bg_path = './bg/1-1.png'
+inputs = os.listdir('input')
 
-intensity_mask = utils.get_brightness_mask(size=size, min_val=0.1)
-bg = cv2.resize(cv2.imread(bg_path), (256, 256))
-# bg = bg * tf.expand_dims(intensity_mask, 2)
-bg = bg * np.expand_dims(intensity_mask.numpy(), 2)
+for inp in inputs:
+    img_format = inp.split('.')[-1]
+    if img_format == 'png':
+        bg = utils.read_float(os.path.join('input', inp),
+                              channel=3, itype='png', is_linear=False)
+    else:
+        bg = utils.read_float(os.path.join('input', inp),
+                              channel=3, itype='jpg', is_linear=False)
+    # create mask
+    min_val = random.uniform(args.min_val, 1)
+    intensity_mask = utils.get_brightness_mask(size=size, min_val=0.7)
+    tf.gfile.FastGFile(os.path.join('mask', inp), intensity_mask)
+    bg = cv2.resize(bg.numpy(), size)
+    new_bg = bg * tf.expand_dims(intensity_mask, 2)
+    tf.gfile.FastGFile(os.path.join('output', inp), new_bg)
 
-
-cv2.imshow('mask', intensity_mask.numpy())
-cv2.imshow('bg', bg * intensity_mask.numpy())
-cv2.waitKey(0)
